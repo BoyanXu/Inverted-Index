b5f1c098c7cd357b385a1c54a4542fe067f3dd5a
BoyanXu <bx376@nyu.edu>
2023-10-22 02:38:23 -0400

    Boilerplate compiles

--- src/decompressor.rs
+++ src/decompressor.rs
@@ -2,8 +2,8 @@ use std::fs::File;
 use std::io::{BufReader, Result, BufRead};
 use flate2::read::GzDecoder;
 
-pub fn decompress_gzip_file(file_path: &str) -> Result<impl BufRead> {
+pub fn decompress_gzip_file(file_path: &str) -> Result<Box<dyn BufRead>> {
     let file = File::open(file_path)?;
     let decoder = GzDecoder::new(file);
-    Ok(BufReader::new(decoder))
+    Ok(Box::new(BufReader::new(decoder)))
 }
