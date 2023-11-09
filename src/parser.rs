40e0de917314a2e7e847cb1c7d7eda76957cca4d
BoyanXu <bx376@nyu.edu>
2023-11-09 16:10:49 -0500

    Change the way assigning doc_id

--- src/parser.rs
+++ src/parser.rs
@@ -4,6 +4,7 @@ use unicode_segmentation::UnicodeSegmentation;
 use stop_words::{get, LANGUAGE::English}; // Import required components from stop-words crate
 use std::collections::HashSet;
 use lazy_static::lazy_static;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 // Cache the stop words for the English language
 lazy_static! {
@@ -13,8 +14,10 @@ lazy_static! {
     };
 }
 
+static DOCID_COUNTER: AtomicUsize = AtomicUsize::new(0);
+
 pub fn parse_document(document: &str) -> (usize, String, Vec<String>) {
-    let doc_id = extract_doc_id(document);
+    let doc_id = get_doc_id(); // extract_doc_id(document);
     let url = extract_url(document);
     let text = extract_text_content(document);
     let tokens = parse_line(&text);
@@ -22,13 +25,18 @@ pub fn parse_document(document: &str) -> (usize, String, Vec<String>) {
     (doc_id, url, tokens)
 }
 
-fn extract_doc_id(document: &str) -> usize { // Change return type to usize
-    let re = Regex::new(r"(?s)<DOCNO>D(\d+)</DOCNO>").unwrap(); // Updated regex to directly match the number after 'D'
-    re.captures(document)
-        .and_then(|cap| cap.get(1))
-        .and_then(|m| m.as_str().parse::<usize>().ok()) // Parse the number to usize
-        .unwrap_or_default()
+// fn extract_doc_id(document: &str) -> usize { // Change return type to usize
+//     let re = Regex::new(r"(?s)<DOCNO>D(\d+)</DOCNO>").unwrap(); // Updated regex to directly match the number after 'D'
+//     re.captures(document)
+//         .and_then(|cap| cap.get(1))
+//         .and_then(|m| m.as_str().parse::<usize>().ok()) // Parse the number to usize
+//         .unwrap_or_default()
+// }
+
+fn get_doc_id() -> usize { // Change return type to usize
+    DOCID_COUNTER.fetch_add(1, Ordering::SeqCst)
 }
+
 
 fn extract_url(document: &str) -> String {
     let text_content = extract_text_content(document);
